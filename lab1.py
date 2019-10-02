32cf4d6c7e641a36e2d505929ea991eacb8fe335
voltako <voltako69@gmail.com>
2019-10-02 07:34:18 +0000

    Моя лаба

---
+++ lab1.py
@@ -0,0 +1,11 @@
+
+from math import *
+a = float(input())
+x= float(input())
+g=(3*(4*a*a +13*a*x+9*x*x))/(10*a*a -51*a*x+5*x*x)
+f=cos(6*a*a +a*x-2*x*x)
+y=acos(14*a*a+37*a*x+5*x*x+1)
+print(g)
+print(f)
+print(y)
+
